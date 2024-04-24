2d842001e420ee48da4ad9dc8f76cf89a6cb7870
Brayden MacEachern <144717636+BananasandCheeese@users.noreply.github.com>
2024-04-24 18:39:16 +0000

    changes

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+//use let instead of var
+console.log("Task 1")
+for (let i=1; i<=10; i++){
+    console.log(i*i)
+}
+console.log("Task 2")
+for (let i=5; i>=1; i--){
+   console.log(i);
+}
+console.log("Blastoff!");
+
+console.log("Task 3")
+for (let i=0; i<=50; i+=2){
+    console.log(i);
+}
+console.log("Task 4")
+for (let i=1; i<=100; i+=1)
+console.log("Task 5")
