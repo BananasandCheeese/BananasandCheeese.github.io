bd519d6652f2b3e688962e05f770435c1ed965f9
Brayden MacEachern <144717636+BananasandCheeese@users.noreply.github.com>
2024-04-26 18:22:16 +0000

    add all tasks.html

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,35 @@ console.log("Task 1")
 for (let i=1; i<=10; i++){
     console.log(i*i)
 }
+
+
 console.log("Task 2")
 for (let i=5; i>=1; i--){
    console.log(i);
 }
 console.log("Blastoff!");
 
+
 console.log("Task 3")
 for (let i=0; i<=50; i+=2){
     console.log(i);
 }
+
+
 console.log("Task 4")
-for (let i=1; i<=100; i+=1)
+let sum = 0
+for (let i = 1; i <= 100; i += 1){
+    sum += i;
+}
+console.log(sum);
+
 console.log("Task 5")
+for (let n = 1; n<=10; n+=1){
+
+    let iFactorial = 1;
+    for (let i=1; i<=n; i+=1){
+        iFactorial *= i;
+     }
+     console.log(iFactorial);
+}
+
